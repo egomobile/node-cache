8974a8de7e23023ff6da85fdb3ff7bf9f2f518aa
Eric Kesseler <eric.kesseler@e-go-mobile.com>
2023-01-24 16:28:49 +0100

    refactor into module structure

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import createServer, { json, query } from "@egomobile/http-server";
-
-const DATASETS: IDataSet[] = [];
-
-export interface IDataSet {
-    key: string;
-    value: string;
-}
+import { createServer as createHttpServer, json, query } from "@egomobile/http-server";
 
 /**
  * Find a dataset by it's key
@@ -19,55 +12,65 @@ function findDataSetByKey(key: string) {
     });
 }
 
-async function main() {
-    const app = createServer();
+const DATASETS: IDataSet[] = [];
 
-    app.put("/datasets", [json()], async (request, response) => {
-        const body: IDataSet = request.body!;
-        if (body.key && body.value) {
-            const dataSet: IDataSet | undefined = findDataSetByKey(body.key);
-            if (dataSet) {
-                const dataSetIndex = DATASETS.indexOf(dataSet);
-                DATASETS[dataSetIndex] = { "key": body.key, "value": body.value };
+export interface IDataSet {
+    key: string;
+    value: string;
+}
+
+export const createServer = async (): Promise<void> => {
+    try {
+        const app = createHttpServer();
+
+        app.put("/datasets", [json()], async (request, response) => {
+            const body: IDataSet = request.body!;
+            if (body.key && body.value) {
+                const dataSet: IDataSet | undefined = findDataSetByKey(body.key);
+                if (dataSet) {
+                    const dataSetIndex = DATASETS.indexOf(dataSet);
+                    DATASETS[dataSetIndex] = { "key": body.key, "value": body.value };
+                }
+                else {
+                    DATASETS.push({ "key": body.key, "value": body.value });
+                }
+                response.writeHead(200);
             }
             else {
-                DATASETS.push({ "key": body.key, "value": body.value });
+                const errorMsg: string = "Both fields, key and value need to be passed e.g.: { 'key': 'foo', 'value': 'bar' }";
+                response.writeHead(400, {
+                    "Content-Length": errorMsg.length
+                });
+                response.write(errorMsg);
             }
-            response.writeHead(200);
-        }
-        else {
-            const errorMsg: string = "Both fields, key and value need to be passed e.g.: { 'key': 'foo', 'value': 'bar' }";
-            response.writeHead(400, {
-                "Content-Length": errorMsg.length
-            });
-            response.write(errorMsg);
-        }
-    });
+        });
 
-    app.get("/datasets", query(), async (request, response) => {
-        const key: any = request.query!.get("key");
-        if (key) {
-            const dataSet: IDataSet | undefined = findDataSetByKey(key);
-            if (dataSet) {
+        app.get("/datasets", query(), async (request, response) => {
+            const key: any = request.query!.get("key");
+            if (key) {
+                const dataSet: IDataSet | undefined = findDataSetByKey(key);
+                if (dataSet) {
+                    response.writeHead(200, {
+                        "Content-Type": "application/json"
+                    });
+                    response.write(JSON.stringify(dataSet));
+                }
+                else {
+                    response.writeHead(404);
+                }
+            }
+            else {
                 response.writeHead(200, {
                     "Content-Type": "application/json"
                 });
-                response.write(JSON.stringify(dataSet));
+                response.write(JSON.stringify(DATASETS));
             }
-            else {
-                response.writeHead(404);
-            }
-        }
-        else {
-            response.writeHead(200, {
-                "Content-Type": "application/json"
-            });
-            response.write(JSON.stringify(DATASETS));
-        }
-    });
+        });
 
-    await app.listen();
-    console.log(`service running on port ${app.port} ...`);
-}
-
-main().catch(console.error);
+        await app.listen();
+        console.log(`service running on port ${app.port} ...`);
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
